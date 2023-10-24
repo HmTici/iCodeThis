c5fbf5ba6b93622f15b056996bcd34eaad63804a
Alex <62963697+HmTici@users.noreply.github.com>
2023-10-24 13:32:40 +0200

    Add files via upload

---
+++ Challenges/12.10.23 Menu 3/script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const ressourcesLink = document.getElementById('ressources');
+  const ressourcesGrid = document.getElementById('ressourcesGrid');
+  const icon = ressourcesLink.querySelector('.rotate-icon');
+
+  ressourcesGrid.style.display = 'none';
+
+  ressourcesLink.addEventListener('click', function () {
+    if (ressourcesGrid.style.display === 'none') {
+      ressourcesGrid.style.display = 'grid';
+      icon.classList.add('rotated');
+    } else {
+      ressourcesGrid.style.display = 'none';
+      ressourcesLink.classList.remove('active');
+      icon.classList.remove('rotated');
+    }
+  });
+});
